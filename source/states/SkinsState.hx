8f622af74354c69f5a7d93244c929dbf09fdda0f
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-08 12:35:12 +0100

    Update SkinsState.hx

--- source/states/SkinsState.hx
+++ source/states/SkinsState.hx
@@ -409,8 +409,8 @@ class SkinsState extends MusicBeatState {
 				}
 				return null;
 			}));
+	      }
 		}
-}
 
 		if (!FlxG.keys.pressed.SHIFT && controls.ACCEPT) {
 			if (selectTimer != null && !selectTimer.finished) {
