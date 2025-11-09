d1a96fe892cc31ac0f9135be56416929ed610b8f
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-11-09 13:46:20 +0100

    Update SkinsState.hx

--- source/states/SkinsState.hx
+++ source/states/SkinsState.hx
@@ -493,9 +493,10 @@ class SkinsState extends MusicBeatState {
 		characterCamera.follow(camFollow, LOCKON, 0.01);
 
 			if (ClientPrefs.data.modSkin.length >= 2) {
+				    FlxG.Text("setSkin", [
 					ClientPrefs.data.modSkin[0],
 					ClientPrefs.data.modSkin[1]
-			 }]);
+			 ]);
 			}
 
 		if (music.playing)
